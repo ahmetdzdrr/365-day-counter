5233b47e373f0fb6650e4dc71318fdce69c95009
Ahmet Dizdar <117534684+ahmetdzdrr@users.noreply.github.com>
2025-01-02 01:04:09 +0300

    Update route.ts

--- app/api/progress/route.ts
+++ app/api/progress/route.ts
@@ -2,26 +2,25 @@ import { NextResponse } from "next/server";
 import { format } from "date-fns";
 
 const getStartDate = () => {
-  const currentYear = new Date().getFullYear(); 
-  return new Date(`${currentYear}-01-02T00:00:00`); 
+  const currentYear = new Date().getFullYear();
+  return new Date(`${currentYear}-01-01T00:00:00`);
 };
 
 export async function GET() {
   const startDate = getStartDate(); 
   const currentDate = new Date();
   
+  let daysPassed = 1;
+
   const endDate = new Date(startDate);
   endDate.setDate(startDate.getDate() + 365);
-
-  const daysPassed = Math.floor(
-    (currentDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24)
-  );
 
   const percentage = (daysPassed / 365) * 100;
   const isComplete = daysPassed >= 365;
 
   if (isComplete) {
     startDate.setFullYear(startDate.getFullYear() + 1);
+    daysPassed = 1;
   }
 
   return NextResponse.json({
